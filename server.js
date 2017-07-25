e23b0caae645ae402eb1e91cdec8626f10ef3311
emontegrande <montegrandeelouiseit1a@gmail.com>
2017-07-25 18:56:40 +0800

    my first commit

--- server.js
+++ server.js
@@ -9,7 +9,10 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(path.join(__dirname, '/')));
 //express routes
 app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'home.html'));
+});
+app.get('/aboutthepage', function(req, res){
+  res.sendFile(path.join(__dirname, 'aboutthepage.html'));
 });
 
 //express server listen
